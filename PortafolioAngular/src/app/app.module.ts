0f7162d878448bbcc965805c849ada42795b902f
Kevin Osorio Suaza <kevin.osoriosuaza@gmail.com>
2021-01-20 05:05:24 -0500

    navbar

--- PortafolioAngular/src/app/app.module.ts
+++ PortafolioAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './portada/portada.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
