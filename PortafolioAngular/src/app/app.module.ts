3eff87a460231efd847eae5b878314bbfbf61c12
Kevin Osorio Suaza <kevin.osoriosuaza@gmail.com>
2021-01-25 05:25:16 -0500

    Home creado y seguimos con las rutas

--- PortafolioAngular/src/app/app.module.ts
+++ PortafolioAngular/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortadaComponent } from './portada/portada.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortadaComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
